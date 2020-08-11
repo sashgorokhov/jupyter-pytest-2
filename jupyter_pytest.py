1cd7f340c7a262f7fda3d710e0ee44b9400e12f7
sashgorokhov <sashgorokhov@gmail.com>
2020-08-11 18:50:36 +0300

    Updates

--- jupyter_pytest.py
+++ jupyter_pytest.py
@@ -3,10 +3,15 @@ from types import ModuleType
 import IPython
 import pytest
 from _pytest import runner
-from _pytest.runner import runtestprotocol
 
 
 class IPythonNamespaceCollector(pytest.Module):
+    """
+    Custom pytest collector that pretends to be a module.
+
+    It collects all user-defined objects from notebook global scope and assigns them to a module object,
+    which is then processed by pytest routines.
+    """
     def _getobj(self):
         mod = ModuleType('notebook_tests')
         shell = IPython.get_ipython()
@@ -17,15 +22,8 @@ class IPythonNamespaceCollector(pytest.Module):
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_make_collect_report(collector):
+    # noop if in non-IPython environment
+    if not IPython.get_ipython():
+        return None
     m = IPythonNamespaceCollector.from_parent(parent=collector, fspath=collector.fspath)
     return runner.pytest_make_collect_report(m)
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_runtest_protocol(item, nextitem):
-    """Copypasted original pytest implementation, except that reports from runtestprotocol are assigned to test item for further access"""
-    item.ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
-    reports = runtestprotocol(item, nextitem=nextitem)
-    item.reports = reports
-    item.ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
-    return True
